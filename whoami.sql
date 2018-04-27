3d16b30349b06e0c8e3e9900a496195b69d3bb6f
pdvmoto <35933355+pdvmoto@users.noreply.github.com>
2018-04-27 21:04:04 +0200

    find who is SID and who you are

---
+++ whoami.sql
@@ -0,0 +1,85 @@
+
+
+set linesize 128
+
+column usrnm		format  A12 trunc
+column osusr		format  A8	trunc
+column machine		format  A15	trunc
+column program		format  A15	trunc
+column process		format  A10
+column spid		format  A5      head spid
+column serial           format 9999     head sernr
+column sid              format 999      head sid
+
+column sidser           format A70
+
+column inst_id         format 9999 head inst
+column inst            format 9999 head inst
+column instance	       format A8   head instance 
+column hname	       format A8   head host     
+column username        format a15
+column logon_time      format a16
+column osuser          format a10
+column machine         format a10
+
+column sidser          format A10 head sidser 
+
+column failover_type   format A7  head fov_typ    trunc   
+column failover_method format A4  head meth       trunc
+column fld_over        format A5  head fldov      trunc
+
+
+
+select 	nvl ( s.username, 'background' )  			usrnm 
+, 		i.instance_name					instance
+, 		i.host_name					hname
+--,             p.inst_id 					inst	
+--, 		s.machine					machine
+,		s.osuser					osusr
+,		'..' || substr ( s.program, 1, 12 )		program
+--,		s.program 					program
+,		p.spid						spid
+--,          to_char(s.logon_time, 'DD-MON HH24:MI:SS')     logon_time
+--,          s.sid
+--,          s.serial#						serial
+, 	     to_char ( s.sid ) || ',' || to_char ( s.serial#)   sidser
+, 	   failover_type
+, 	   failover_method
+, 	   failed_over fld_over
+--,          'exec SYS.DBMS_SYSTEM.SET_EV( '
+--	   || to_char ( s.sid ) || ',' || to_char ( s.serial#) 
+--	   || ', 12, ''''); 'sidser
+from v$session 	s
+,	v$process	p
+, 	v$instance	i
+where	1=1
+and 	s.paddr = p.addr
+--and     s.inst_id = p.inst_id
+and s.sid in ( select sid from v$mystat where rownum < 2)
+order by s.username, s.osuser
+/
+
+
+select 
+  s.inst_id                                             inst
+, nvl ( s.username, 'background' )  			usrnm 
+, count (*)                                             nr_connects
+from gv$session s
+group by inst_id, username
+/
+
+-- experiment with CDB
+column con_id 		format 999
+column con_name		format a10
+
+select 
+  s.inst_id                                             inst
+, nvl ( s.username, 'background' )  			usrnm 
+, s.con_id						con_id
+, nvl ( p.name, 'cdb_root')				con_name 
+, count (*)                                             nr_connects
+from gv$session s
+, gv$pdbs p
+where p.con_id (+)= s.con_id 
+group by s.inst_id, s.username, s.con_id, p.name
+/
---
+++ whois.sql
@@ -0,0 +1,88 @@
+
+-- find deatils on sid 
+
+
+column username format A10
+column program  format A25 
+column terminal format a10
+column osuser   format a10
+column logontim format a15
+column uxpid    format 99999 head uxpid
+column machine  format A20
+
+column whoisout format A70 newline
+
+column waitsid                       format 9999999
+column usr_osusr_mchn_term_prgr_evnt format A70
+
+col event for a35
+set pages 60
+
+select s.sid
+, s.username
+, p.program
+, p.terminal
+, s.osuser
+, p.spid  as uxpid
+, to_char ( s.logon_time, 'MON-DD HH24:MI:SS' ) as logontim
+, s.machine
+from v$session s
+   , v$process p 
+where s.paddr = p.addr(+)
+  and s.sid = &1
+/
+
+set head off
+
+select 
+  'Column          Value'
+, '--------------  -----------'  as whoisout
+, 's.SID, s.Sess : ' || s.sid || ',' || s.serial#   as whoisout
+, 's.Username    : ' || s.username                  as whoisout
+, 's.osuser      : ' || s.osuser                    as whoisout
+, 'p.Program     : ' || p.program                   as whoisout
+, 'p.Terminal    : ' || p.terminal                  as whoisout
+, 'p.pid, p.spid : ' || p.pid || ',' ||  p.spid     as whoisout
+-- , 'p.spid        : ' || p.spid                   as whoisout
+, 's.logontime   : ' || to_char ( s.logon_time, 'MON-DD HH24:MI:SS' ) as whoisout
+, 's.machine     : ' || s.machine                   as whoisout
+, 's.type        : ' || s.type                      as whoisout
+, 's.module      : ' || s.module                    as whoisout
+, 's.client_info : ' || s.client_info               as whoisout
+, 's.sqladdr+hsh : ' || s.sql_address || ',' || s.sql_hash_value       as whoisout 
+--, 's.event       : ' || s.event || ' ('|| s.wait_class || ')'        as whoisout
+--, 's.blckng_ses  : ' || s.blocking_session          as whoisout
+from v$session s
+   , v$process p 
+where s.paddr = p.addr(+)
+  and s.sid = &1
+/
+
+
+
+select 
+  'Column          Value'
+, '--------------  -----------'  as whoisout
+, 's.event       : ' || s.event || ' ('|| s.wait_class || ')'                           as whoisout
+, 's.blckng_ses  : ' || nvl ( to_char ( s.blocking_session), 'none' )                   as whoisout
+, ' '     as whoisout
+, 'alter system kill session ''' || s.sid ||         ',' || s.serial#        || ''';'   as whoisout
+, 'exec dbms_shared_pool.purge  ( ''' || s.sql_address || ',' || s.sql_hash_value || ''', ''C'');'   as whoisout 
+from v$session s
+   , v$process p 
+where s.paddr = p.addr(+)
+  and s.sid = &1
+/
+set head on
+
+
+select waiting_session waitsid
+--, holding_session 
+, s.schemaname || '|' || s.osuser || ' @ ' || s.machine|| '|' || s.terminal|| '|' ||s.program 
+/* || '|'||  s.event */ AS usr_osusr_mchn_term_prgr_evnt
+--, s.*
+from dba_waiters  w
+, v$session s
+where w.waiting_session = s.sid
+and w.holding_session = &1
+order by holding_session, waiting_session ; 
