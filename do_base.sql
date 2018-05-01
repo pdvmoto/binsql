c5896de101f8aea4c30b1b133e32f271f2674577
pdvmoto <35933355+pdvmoto@users.noreply.github.com>
2018-05-01 09:05:07 +0200

    run test for some time, generate timestamps

---
+++ do_base.sql
@@ -0,0 +1,6 @@
+
+insert into robert.data_chk values
+(  robert.data_chk_seq.nextval, sysdate, 'next scripted insert...' ) ;
+
+commit ;
+
---
+++ do_base_hr.sql
@@ -0,0 +1,15 @@
+
+@do_base
+
+host sleep 30
+
+@do_base
+
+host sleep 30
+
+@do_base
+
+host sleep 30
+
+@do_base_hr
+
